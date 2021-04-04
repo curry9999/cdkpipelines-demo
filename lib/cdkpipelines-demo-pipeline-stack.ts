b4a5ff69bc66bd31003c381977605c8d22df975a
curry9999 <curry123456789@gmail.com>
2021-04-04 11:46:25 +0900

    initial

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -35,7 +35,7 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
          
          // We need a build step to compile the TypeScript Lambda
          buildCommand: 'echo',
-         installCommand: 'echo',
+         installCommand: 'npm insatall',
          synthCommand: 'echo',
         }),
     });
