fa1f20585fd1b7ecb1b8c81d7a68278486ce15b3
curry9999 <curry123456789@gmail.com>
2021-04-04 11:54:35 +0900

    initial

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -35,7 +35,7 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
          
          // We need a build step to compile the TypeScript Lambda
          buildCommand: 'echo',
-         installCommand: 'npm install && npm run build && cdk synth',
+         installCommand: 'npm cache clear && npm prune && npx projen && npm install && ls node_modules/@aws-cdk/aws-backup/lib && npx projen build',
          synthCommand: 'echo',
         }),
     });
